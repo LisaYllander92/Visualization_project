dd9f96c754bc7f43f1e24896af790371b836faab
LisaYllander92 <Lisaylander92@gmail.com>
2026-04-08 12:44:05 +0200

    update: tabels in init.sql

--- init.sql
+++ init.sql
@@ -1,31 +1,51 @@
 -- table to store raw data
-CREATE TABLE IF NOT EXISTS staging_events (
-    id SERIAL PRIMARY KEY,
-    raw_data TEXT NOT NULL,
-    ingested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+        CREATE TABLE IF NOT EXISTS events_raw (
+            event_id        VARCHAR PRIMARY KEY,
+            name            VARCHAR,
+            url             VARCHAR,
+            image_url       VARCHAR,
+            date            DATE,
+            time            VARCHAR,
+            status          VARCHAR,
+            venue_id        VARCHAR,
+            category_id     INTEGER,
+            segment         VARCHAR,
+            genre           VARCHAR,
+            subgenre        VARCHAR,
+            venue_name      VARCHAR,
+            venue_city      VARCHAR,
+            venue_address   VARCHAR,
+            venue_lat       DOUBLE PRECISION,
+            venue_lon       DOUBLE PRECISION
+        );
 
 -- table to store cleaned data
-CREATE TABLE IF NOT EXISTS curated_events (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    url TEXT NOT NULL,
-    image TEXT,
-    date DATE,
-    time TIMESTAMP,
-    status TEXT,
-    segment TEXT,
-    genre TEXT,
-    subgenre TEXT,
-    venue_name TEXT,
-    adress TEXT,
-    venue_lat FLOAT,
-    venue_lon FLOAT
-);
+        CREATE TABLE IF NOT EXISTS events_clean (
+            event_id        VARCHAR PRIMARY KEY,
+            name            VARCHAR NOT NULL,
+            url             VARCHAR,
+            image_url       VARCHAR,
+            date            DATE,
+            time            VARCHAR,
+            status          VARCHAR,
+            segment         VARCHAR,
+            genre           VARCHAR,
+            subgenre        VARCHAR,
+            venue_name      VARCHAR,
+            venue_city      VARCHAR,
+            venue_address   VARCHAR,
+            venue_lat       DOUBLE PRECISION,
+            venue_lon       DOUBLE PRECISION,
+            day_of_week     VARCHAR,
+            month_name      VARCHAR,
+            month_num       INTEGER,
+            year            INTEGER,
+            hour            INTEGER
+        );
 
 -- table to store search events (for statistics)
-CREATE TABLE IF NOT EXISTS events_search (
-    id SERIAL PRIMARY KEY,
-    query VARCHAR(255) NOT NULL,
-    searched_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+-- CREATE TABLE IF NOT EXISTS events_search (
+--    id SERIAL PRIMARY KEY,
+--    query VARCHAR(255) NOT NULL,
+--    searched_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

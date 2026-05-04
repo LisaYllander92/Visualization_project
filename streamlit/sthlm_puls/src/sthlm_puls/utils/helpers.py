c4a08e0cd7028cb2a8f29152b52cee102eb9d015
LisaYllander92 <Lisaylander92@gmail.com>
2026-05-04 10:05:33 +0200

    streamlit folder stucture

---
+++ streamlit/sthlm_puls/src/sthlm_puls/utils/helpers.py
@@ -0,0 +1,20 @@
+from sthlm_puls.utils.constants import DATA_PATH
+import pandas as pd
+import streamlit as st
+
+
+def read_textfile(path):
+    with open(path) as file:
+        return file.read()
+
+
+@st.cache_data
+def get_events_df():
+    return pd.read_csv(DATA_PATH / "events_combined.csv")
+
+
+def read_css(path):
+    css = read_textfile(path)
+    st.write(
+        f"<style>{css}</style>", unsafe_allow_html=True
+    )

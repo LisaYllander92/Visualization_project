27e6f14f2dc052ad22482800a12ec563ff19a87e
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-05-05 11:04:28 +0200

    Remove streamlit folder, moved to separate repo

--- streamlit/sthlm_puls/src/sthlm_puls/pages/raw_data.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from sthlm_puls.utils.helpers import get_events_df
-
-def raw_data():
-    st.markdown("# RAW DATA")
-    st.dataframe(get_events_df())
-
-if __name__ == "__main__":
-    raw_data()
